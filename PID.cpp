2d9759905524c4e05fbbec9bf676f9682df15c15
jamate <mateoarcilalopez@gmail.com>
2023-03-07 20:53:42 -0500

    Basic implementation of PI controller

--- PID.cpp
+++ PID.cpp
@@ -1,1 +1,55 @@
+#include <cmath>
+
 #include "PID.h"
+
+PIDController::PIDController(PIDType type, float K, float tau, float tss, float OS) {
+    OS /= 100; // Overshoot is passed in percentage, need it as a decimal
+    float zeta = std::sqrt((std::pow(std::log(OS), 2)) / (std::pow(M_PI, 2) + std::pow(std::log(OS), 2)));
+    float wn = 4 / (zeta*tss); // 2% criterion. 98% of steady state value
+
+    // Controller constants based on pole placement
+    kp = (2*zeta*wn*tau - 1) / K;
+    ki = tau*std::pow(wn, 2) / K;
+
+    // q0 = -kp;
+    // q1 = kp+ki*Ts;
+    ;
+}
+
+float PIDController::calculateControl(float error) {
+    static absolute_time_t last_time {get_absolute_time()};
+    absolute_time_t current_time = get_absolute_time();
+    float delta_time = absolute_time_diff_us(current_time, last_time) * 1e-6f;
+
+    float output {0};
+    switch (PID_type) {
+        case PIDType::P_pole_placement_closed_loop_plant:
+            output = pCalculateControl(error, delta_time);
+            break;
+        case PIDType::PI_pole_placement_1st_order_plant:
+            output = piCalculateControl(error, delta_time);
+            break;
+        case PIDType::PID_pole_placement_2nd_order_plant:
+            output = pidPpCalculateControl(error, delta_time);
+            break;
+        case PIDType::PID_manual_tunning:
+            output = pidMCalculateControl(error, delta_time);
+            break;
+    }
+
+    last_output = output;
+    last_error = error;
+    last_time = current_time;
+    return output;
+}
+
+float PIDController::piCalculateControl(float error, float delta_time) {
+    /// \todo Separate each term in order to implement anti-windup.
+    // Controller transfer function: U(s)/E(s) = kp + ki/s
+    static float q0 = -kp;
+    static float q1 = kp + ki*delta_time;
+
+    float output = q1*error + q0*last_error + last_output;
+
+    return output;
+}
--- PID.h
+++ PID.h
@@ -1,8 +1,24 @@
 #pragma once
 
+#include <cmath>
+
+#include "pico/stdlib.h"
+
+enum class PIDType {
+    P_pole_placement_closed_loop_plant,
+    PI_pole_placement_1st_order_plant,
+    PID_pole_placement_2nd_order_plant,
+    PID_manual_tunning
+};
+
 class PIDController {
 public: // Public attributes
-    
+    PIDType PID_type;
+
+    float kp; /// Proportional term gain.
+    float ki; /// Integral term gain.
+    float kd; /// Derivative term gain (careful, usually is not needed because the signal from usually encoders has zero to no noise).
+    float N; /// Derivative filter coefficient. Use only when derivative term gain is different from zero. \todo implement PID with filter on the derivative term
 
 private: // Private attributes
     float last_error;
@@ -10,7 +26,46 @@ private: // Private attributes
     float last_integral;
 
 public: // Public methods
-    PIDController();
+    /**
+     * @brief This function creates a new PIDController object that automatically calculates the controller 
+     * constants based on the given first-order motor model and desired response parameters.
+     * The input to the model of the motor has to be Duty cycle of a PWM signal.
+     * 
+     * @param type For calling this constructor the type has to be PIDType::PI_pole_placement_1st_order_plant.
+     * @param K Gain of the first order system representing the motor.
+     * @param tau [s]. Time constant of the first order system representing the motor.
+     * @param tss [s]. Desired settling time for the controlled system.
+     * @param OS [%]. Desired overshoot for the controlled system.
+     */
+    PIDController(PIDType type, float K, float tau, float tss, float OS);
 
-    calculateControl(float error);
+    /**
+     * @brief This function creates a new PIDController object that automatically calculates the controller 
+     * constants based on the given second-order motor model and desired response parameters.
+     * The input to the model of the motor has to be Duty cycle of a PWM signal.
+     * 
+     * @param type For calling this constructor the type has to be PIDType::PID_pole_placement_2nd_order_plant.
+     * @param K Gain of the second order system representing the motor.
+     * @param zeta Dampening ratio of the second order system representing the motor.
+     * @param wn Undamped natural frequency of the second order system representing the motor.
+     * @param tss [s]. Desired settling time for the controlled system.
+     * @param OS [%]. Desired overshoot for the controlled system.
+     */
+    PIDController(PIDType type, float K, float zeta, float wn, float tss, float OS);
+
+    /**
+     * @brief 
+     * 
+     * @param error (sp-pv). Difference between desired motor output and actual motor output.
+     * @return DC [%]. Duty cycle of the PWM. The magnitude dictates strength of movement, the sign dictates the direction.
+     */
+    float calculateControl(float error);
+
+private: // Private methods
+
+    float pCalculateControl(float error, float delta_time);
+    float piCalculateControl(float error, float delta_time);
+    float pidPpCalculateControl(float error, float delta_time);
+    float pidMCalculateControl(float error, float delta_time);
+
 };
